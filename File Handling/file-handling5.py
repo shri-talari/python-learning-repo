b18f71ea766e565992394297a8ca1ef1d4f1f45f
Shri Talari <talari.shri@gmail.com>
2025-09-12 19:35:30 +0530

    Added Solved Q5

---
+++ File Handling/file-handling5.py
@@ -0,0 +1,27 @@
+'''Q5. Quiz Game with File Storage
+
+Problem:
+Create a quiz game using file handling.
+
+Questions are stored in a file (questions.txt) in format:
+"question|option1,option2,option3|answer"
+
+Program reads questions and asks user.
+
+User’s answers are stored in answers.txt.
+
+At the end, show:
+
+Score.
+
+Correctly answered questions (set).
+
+Summary dictionary {question: chosen_answer}.
+
+Handle exceptions:
+
+Invalid file format.
+
+File not found.
+
+Invalid user input.'''
